2d069958f396387c3a7899e9642180e7887db8ca
Inigo Lopez <inigolcanalejo@gmail.com>
2019-03-27 17:36:41 +0100

    using potential_flow_analysis_refinement

--- MeshRefinement/validation/settings/set_paths.sh
+++ MeshRefinement/validation/settings/set_paths.sh
@@ -4,10 +4,10 @@ sed 's|'"salome_output_path = 'TBD'"'|'"salome_output_path = '$salome_output_pat
 /$Salome_Converter_File_Path /$Generate_Mesh_Cosine_File_Path 
 
 sed 's|'"mdpa_path = 'TBD'"'|'"mdpa_path = '$mdpa_path'"'|g' -i \
-/$Salome_Converter_File_Path /$Mesh_Domain_Refinement_File_Path
+/$Salome_Converter_File_Path /$Potential_Flow_Analysis_File_Path
 
 sed 's|'"gid_output_path = 'TBD'"'|'"gid_output_path = '$gid_output_path'"'|g' -i \
-/$Mesh_Domain_Refinement_File_Path
+/$Potential_Flow_Analysis_File_Path
 
 sed 's|'"input_dir_path = 'TBD'"'|'"input_dir_path = '$input_dir_path'"'|g' -i \
 /$Compute_Lift_Process
