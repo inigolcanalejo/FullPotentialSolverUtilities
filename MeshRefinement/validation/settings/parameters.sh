2c4986804304ad4ca83cb1b67c5d42006a6e4117
Inigo Lopez <inigolcanalejo@gmail.com>
2019-06-28 13:37:42 +0200

    new parameters

--- MeshRefinement/validation/settings/parameters.sh
+++ MeshRefinement/validation/settings/parameters.sh
@@ -3,14 +3,14 @@ Number_Of_Domains_Size=1
 Number_Of_Refinements=3
 Number_Of_AOAS=1
 
-Initial_Domain_Size=1e3
+Initial_Domain_Size=100
 Domain_Size_Factor=10
 
 Initial_AOA=5.0
 AOA_Increment=1.0
 
-Initial_Airfoil_MeshSize=1e-3
+Initial_Airfoil_MeshSize=1e-2
 Airfoil_Refinement_Factor=0.1
 
-Initial_FarField_MeshSize=2e6
+Initial_FarField_MeshSize=1.0
 FarField_Refinement_Factor=1.0
