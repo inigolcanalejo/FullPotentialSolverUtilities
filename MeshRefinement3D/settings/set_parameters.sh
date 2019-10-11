42579ae7a07868724068b150ae0bfbd91675761f
Inigo Lopez <inigolcanalejo@gmail.com>
2019-10-11 14:31:52 +0200

    updating set parameters

--- MeshRefinement3D/settings/set_parameters.sh
+++ MeshRefinement3D/settings/set_parameters.sh
@@ -1,64 +1,37 @@
 Generate_Mesh_File_Path=$PWD/generate_mdpas/generate_finite_wing.py
+Salome_Converter_File_Path=$PWD/generate_mdpas/use_converter.py
+Potential_Flow_Analysis_File_Path=$PWD/runKratos/potential_flow_analysis_refinement.py
+
 Generate_Mesh_Cosine_File_Path=$PWD/generate_mdpas/generateMeshRefinementCosine.py
 Generate_Mesh_Cosine_Wake_File_Path=$PWD/generate_mdpas/generateMeshRefinementCosineWake.py
 Generate_Mesh_Membrane_File_Path=$PWD/generate_mdpas/membraneAirfoil_separating4.py
-Salome_Converter_File_Path=$PWD/generate_mdpas/use_converter.py
 Salome_Converter_Membrane_File_Path=$PWD/generate_mdpas/use_converter_membrane.py
-Potential_Flow_Analysis_File_Path=$PWD/runKratos/potential_flow_analysis_refinement.py
+
 
 source settings/set_paths.sh
 
-sed 's|'"Number_Of_Wing_Refinements = TBD"'|'"Number_Of_Wing_Refinements = $Number_Of_Wing_Refinements"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
+declare -A Parameters
 
-sed 's|'"Number_Of_AOAS = TBD"'|'"Number_Of_AOAS = $Number_Of_AOAS"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
+Parameters[Number_Of_AOAS]=$Number_Of_AOAS
+Parameters[Number_Of_Domains_Refinements]=$Number_Of_Domains_Refinements
+Parameters[Number_Of_Wing_Refinements]=$Number_Of_Wing_Refinements
 
-sed 's|'"Number_Of_Domains_Refinements = TBD"'|'"Number_Of_Domains_Refinements = $Number_Of_Domains_Refinements"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
+Parameters[Initial_AOA]=$Initial_AOA
+Parameters[AOA_Increment]=$AOA_Increment
 
+Parameters[Initial_Growth_Rate_Domain]=$Initial_Growth_Rate_Domain
+Parameters[Growth_Rate_Domain_Refinement_Factor]=$Growth_Rate_Domain_Refinement_Factor
 
-sed 's|'"Initial_AOA = TBD"'|'"Initial_AOA = $Initial_AOA"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
+Parameters[Initial_Growth_Rate_Wing]=$Initial_Growth_Rate_Wing
+Parameters[Growth_Rate_Wing_Refinement_Factor]=$Growth_Rate_Wing_Refinement_Factor
 
-sed 's|'"AOA_Increment = TBD"'|'"AOA_Increment = $AOA_Increment"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
+Parameters[Smallest_Airfoil_Mesh_Size]=$Smallest_Airfoil_Mesh_Size
+Parameters[Biggest_Airfoil_Mesh_Size]=$Biggest_Airfoil_Mesh_Size
+Parameters[Wing_span]=$Wing_span
 
-
-
-sed 's|'"Initial_Growth_Rate_Wing = TBD"'|'"Initial_Growth_Rate_Wing = $Initial_Growth_Rate_Wing"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Growth_Rate_Wing_Refinement_Factor = TBD"'|'"Growth_Rate_Wing_Refinement_Factor = $Growth_Rate_Wing_Refinement_Factor"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-
-
-sed 's|'"Smallest_Airfoil_Mesh_Size = TBD"'|'"Smallest_Airfoil_Mesh_Size = $Smallest_Airfoil_Mesh_Size"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Biggest_Airfoil_Mesh_Size = TBD"'|'"Biggest_Airfoil_Mesh_Size = $Biggest_Airfoil_Mesh_Size"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Wing_span = TBD"'|'"Wing_span = $Wing_span"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-
-sed 's|'"Initial_Growth_Rate_Domain = TBD"'|'"Initial_Growth_Rate_Domain = $Initial_Growth_Rate_Domain"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Growth_Rate_Domain_Refinement_Factor = TBD"'|'"Growth_Rate_Domain_Refinement_Factor = $Growth_Rate_Domain_Refinement_Factor"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
+#echo 'Parameters:'
+for path_variable_name in "${!Parameters[@]}"; do
+    #echo ' '$path_variable_name '=' ${Parameters[$path_variable_name]}
+    sed 's|'"$path_variable_name = TBD"'|'"$path_variable_name = ${Parameters[$path_variable_name]}"'|g' -i \
+    /$Generate_Mesh_File_Path /$Salome_Converter_File_Path /$Potential_Flow_Analysis_File_Path
+done
--- MeshRefinement3D/settings/unset_parameters.sh
+++ MeshRefinement3D/settings/unset_parameters.sh
@@ -1,53 +1,7 @@
 
 source settings/unset_paths.sh
 
-sed 's|'"Number_Of_Wing_Refinements = $Number_Of_Wing_Refinements"'|'"Number_Of_Wing_Refinements = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Number_Of_AOAS = $Number_Of_AOAS"'|'"Number_Of_AOAS = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Number_Of_Domains_Refinements = $Number_Of_Domains_Refinements"'|'"Number_Of_Domains_Refinements = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-
-sed 's|'"Initial_AOA = $Initial_AOA"'|'"Initial_AOA = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"AOA_Increment = $AOA_Increment"'|'"AOA_Increment = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-
-
-sed 's|'"Initial_Growth_Rate_Wing = $Initial_Growth_Rate_Wing"'|'"Initial_Growth_Rate_Wing = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Growth_Rate_Wing_Refinement_Factor = $Growth_Rate_Wing_Refinement_Factor"'|'"Growth_Rate_Wing_Refinement_Factor = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Smallest_Airfoil_Mesh_Size = $Smallest_Airfoil_Mesh_Size"'|'"Smallest_Airfoil_Mesh_Size = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Biggest_Airfoil_Mesh_Size = $Biggest_Airfoil_Mesh_Size"'|'"Biggest_Airfoil_Mesh_Size = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Wing_span = $Wing_span"'|'"Wing_span = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Initial_Growth_Rate_Domain = $Initial_Growth_Rate_Domain"'|'"Initial_Growth_Rate_Domain = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
-
-sed 's|'"Growth_Rate_Domain_Refinement_Factor = $Growth_Rate_Domain_Refinement_Factor"'|'"Growth_Rate_Domain_Refinement_Factor = TBD"'|g' -i /$Generate_Mesh_File_Path \
-                                /$Salome_Converter_File_Path  /$Generate_Mesh_Cosine_File_Path /$Generate_Mesh_Cosine_Wake_File_Path \
-                                /$Potential_Flow_Analysis_File_Path /$Generate_Mesh_Membrane_File_Path /$Salome_Converter_Membrane_File_Path
+for path_variable_name in "${!Parameters[@]}"; do
+    sed 's|'"$path_variable_name = ${Parameters[$path_variable_name]}"'|'"$path_variable_name = TBD"'|g' -i \
+    /$Generate_Mesh_File_Path /$Salome_Converter_File_Path /$Potential_Flow_Analysis_File_Path
+done
