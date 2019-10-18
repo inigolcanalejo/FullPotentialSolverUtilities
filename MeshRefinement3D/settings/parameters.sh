0bb922d37e295f56aca2c51b5e124a439c30402c
Inigo Lopez <inigolcanalejo@gmail.com>
2019-10-18 18:36:23 +0200

    running small case

--- MeshRefinement3D/settings/parameters.sh
+++ MeshRefinement3D/settings/parameters.sh
@@ -1,7 +1,7 @@
 #Parameters
 Number_Of_AOAS=1
 Number_Of_Domains_Refinements=1
-Number_Of_Wing_Refinements=7
+Number_Of_Wing_Refinements=2
 
 Initial_AOA=5.0
 AOA_Increment=1.0
@@ -12,6 +12,6 @@ Growth_Rate_Domain_Refinement_Factor=0.1
 Initial_Growth_Rate_Wing=0.7
 Growth_Rate_Wing_Refinement_Factor=0.1
 
-Smallest_Airfoil_Mesh_Size=0.001
+Smallest_Airfoil_Mesh_Size=0.01
 Biggest_Airfoil_Mesh_Size=0.05
 Wing_span=4.0
