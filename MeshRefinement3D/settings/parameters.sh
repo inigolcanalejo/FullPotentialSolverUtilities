538ed356e931a9f1493a8a7b071f1baca9616669
Inigo Lopez <inigolcanalejo@gmail.com>
2019-10-25 19:25:33 +0200

    refinement

--- MeshRefinement3D/settings/parameters.sh
+++ MeshRefinement3D/settings/parameters.sh
@@ -1,17 +1,17 @@
 #Parameters
 Number_Of_AOAS=1
-Number_Of_Domains_Refinements=1
-Number_Of_Wing_Refinements=1
+Number_Of_Domains_Refinements=3
+Number_Of_Wing_Refinements=6
 
 Initial_AOA=5.0
 AOA_Increment=1.0
 
-Initial_Growth_Rate_Domain=0.7
-Growth_Rate_Domain_Refinement_Factor=0.1
+Initial_Growth_Rate_Domain=0.64
+Growth_Rate_Domain_Refinement_Factor=2.0
 
-Initial_Growth_Rate_Wing=0.64
+Initial_Growth_Rate_Wing=0.32
 Growth_Rate_Wing_Refinement_Factor=2.0
 
-Smallest_Airfoil_Mesh_Size=0.01
+Smallest_Airfoil_Mesh_Size=0.032
 Biggest_Airfoil_Mesh_Size=0.05
 Wing_span=4.0
