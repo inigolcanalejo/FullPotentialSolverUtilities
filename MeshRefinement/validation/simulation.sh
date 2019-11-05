8a736175bf42f019b125aa641c0753a6b1a863fd
Inigo Lopez <inigolcanalejo@gmail.com>
2019-11-05 14:03:23 +0100

    running single lagrange multipliers

--- MeshRefinement/validation/simulation.sh
+++ MeshRefinement/validation/simulation.sh
@@ -20,14 +20,14 @@ source settings/parameters.sh
 source settings/set_parameters.sh
 cd generate_mdpas/
 
-# Run salome: generate geometry and mesh
-rm $input_dir_path/output_salome/*
-python3 runSalome.py
+# # Run salome: generate geometry and mesh
+# rm $input_dir_path/output_salome/*
+# python3 runSalome.py
 
-# Convert salomes mesh into mdpa
-rm $input_dir_path/mdpas/*
-python3 use_converter.py
-#python3 use_converter_membrane.py
+# # Convert salomes mesh into mdpa
+# rm $input_dir_path/mdpas/*
+# python3 use_converter.py
+# #python3 use_converter_membrane.py
 
 # # Save mdpas file in a copy
 # #source save_mdpas.sh
