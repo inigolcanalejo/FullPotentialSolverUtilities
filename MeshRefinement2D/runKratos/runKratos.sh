588f89c518b616ba3db43c0deccafe54835da953
Inigo Lopez <inigolcanalejo@gmail.com>
2020-01-14 14:38:21 +0100

    cleaning preamble

--- MeshRefinement2D/runKratos/runKratos.sh
+++ MeshRefinement2D/runKratos/runKratos.sh
@@ -3,6 +3,13 @@ source scripts/removing_before_kratos_run.sh
 
 cd runKratos/
 
+GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+DATE=`date '+%Y%m%d_%H%M%S'`
+FILE=${input_dir_path}/plots/output_terminal.txt
+NAME=${FILE%.*}
+EXT=${FILE#*.}
+NEWFILE=${NAME}_${DATE}_${GITBRANCH}.${EXT}
+
 # Run Kratos
 unbuffer python3 MeshDomainRefinement.py 2>&1 | tee $NEWFILE
 
--- MeshRefinement2D/scripts/copy_results.sh
+++ MeshRefinement2D/scripts/copy_results.sh
@@ -1,14 +1,11 @@
-##Copying results
-DIRECTORY=/media/inigo/10740FB2740F9A1C/Results/COUPLED_PROBLEMS/05_MeshRefinement
+# Copying results
 DATE=`date '+%Y%m%d_%H%M%S'`
-mkdir -p ${DIRECTORY}_${DATE}_${GITBRANCH}_${OUTPUTNAME}
-mkdir -p ${DIRECTORY}_${DATE}_${GITBRANCH}_${OUTPUTNAME}/output_gid
-mkdir -p ${DIRECTORY}_${DATE}_${GITBRANCH}_${OUTPUTNAME}/mdpas
+output_dir_complete_path=${output_dir_path}_${DATE}_${GITBRANCH}_${OUTPUTNAME}
+mkdir -p $output_dir_complete_path
 
-cp -r $input_dir_path/plots/ ${DIRECTORY}_${DATE}_${GITBRANCH}_${OUTPUTNAME}
-cp -r /media/inigo/10740FB2740F9A1C/Outputs/05_MeshRefinement/* ${DIRECTORY}_${DATE}_${GITBRANCH}_${OUTPUTNAME}/output_gid
+cp -r $input_dir_path/mdpas $output_dir_complete_path/mdpas
+cp -r $input_dir_path/output_gid $output_dir_complete_path/output_gid
+cp -r $input_dir_path/output_salome $output_dir_complete_path/output_salome
+cp -r $input_dir_path/plots $output_dir_complete_path/plots
 
-#Save the mdpas in a copy
-cp -r $input_dir_path/mdpas/n* ${DIRECTORY}_${DATE}_${GITBRANCH}_${OUTPUTNAME}/mdpas
-
-cp $PWD/settings/parameters.sh ${DIRECTORY}_${DATE}_${GITBRANCH}_${OUTPUTNAME}
+cp $PWD/settings/parameters.sh $output_dir_complete_path
