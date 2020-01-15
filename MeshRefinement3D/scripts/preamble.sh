eea2a3b8de5ca2737c98bd0df4dd0e24e78e4c93
Inigo Lopez <inigolcanalejo@gmail.com>
2020-01-15 17:17:13 +0100

    adding mesh generation

--- MeshRefinement3D/scripts/preamble.sh
+++ MeshRefinement3D/scripts/preamble.sh
@@ -5,8 +5,8 @@ export LD_LIBRARY_PATH=""
 #Setting paths
 source /home/inigo/Documents/paths/salomeConverter.sh
 #source /home/inigo/Documents/paths/kratosMaster3.sh
-#source /home/inigo/Documents/paths/kratosMaster.sh
-source /home/inigo/Documents/paths/kratosMaster4.sh
+source /home/inigo/Documents/paths/kratosMaster.sh
+#source /home/inigo/Documents/paths/kratosMaster4.sh
 #source /home/inigo/Documents/paths/kratosMerge.sh
 #source /home/inigo/intel/mkl/bin/mklvars.sh intel64 lp64
 
@@ -15,7 +15,7 @@ echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 
 GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 #echo "GITBRANCH = $GITBRANCH"
-input_dir_path=/media/inigo/10740FB2740F9A1C/3d_results
+input_dir_path=/media/inigo/10740FB2740F9A1C/3d_results_test
 #input_dir_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/20191011_101705_AOA_0.0_Wing_Span_4.0_Airfoil_Mesh_Size_0.01_Biggest_Airfoil_Mesh_Size_0.05_Initial_Growth_Rate_Domain_0.7_Initial_Growth_Rate_Wing_0.7
 salome_output_path=$input_dir_path/output_salome
 mdpa_path=$input_dir_path/mdpas
@@ -37,3 +37,4 @@ EXT=${FILEK#*.}
 FILEKRATOS=${NAMEK}_${DATE}_${GITBRANCH}.${EXT}
 FILESALOME=${NAMES}_${DATE}_${GITBRANCH}.${EXT}
 FILECONVERTER=${NAMEC}_${DATE}_${GITBRANCH}.${EXT}
+mkdir -p ${input_dir_path}/plots
