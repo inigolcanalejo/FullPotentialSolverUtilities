5085fa297564115d1dc3a706a965e4a56b72592a
Inigo Lopez <inigolcanalejo@gmail.com>
2020-01-23 14:59:23 +0100

    cleaning preamble 3d

--- MeshRefinement3D/scripts/preamble.sh
+++ MeshRefinement3D/scripts/preamble.sh
@@ -1,30 +1,27 @@
-#Resetting paths
+# Resetting paths
 export PYTHONPATH=""
 export LD_LIBRARY_PATH=""
 
-#Setting paths
+# Setting paths
 source /home/inigo/Documents/paths/salomeConverter.sh
-#source /home/inigo/Documents/paths/kratosMaster3.sh
-source /home/inigo/Documents/paths/kratosMaster.sh
-#source /home/inigo/Documents/paths/kratosMaster4.sh
-#source /home/inigo/Documents/paths/kratosMerge.sh
+source /home/inigo/Documents/paths/kratosMaster4.sh
 #source /home/inigo/intel/mkl/bin/mklvars.sh intel64 lp64
 
 echo "PYTHONPATH = $PYTHONPATH"
 echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 
 GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-#echo "GITBRANCH = $GITBRANCH"
+
+# Path where mdpas and outputs are created
 input_dir_path=/media/inigo/10740FB2740F9A1C/3d_results_test
-#input_dir_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/20191011_101705_AOA_0.0_Wing_Span_4.0_Airfoil_Mesh_Size_0.01_Biggest_Airfoil_Mesh_Size_0.05_Initial_Growth_Rate_Domain_0.7_Initial_Growth_Rate_Wing_0.7
+
+# Path where mdpas and output are saved
+output_dir_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/
+
+# Further paths
 salome_output_path=$input_dir_path/output_salome
 mdpa_path=$input_dir_path/mdpas
-#mdpa_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/20191022_122442_AOA_5.0_Wing_Span_4.0_Airfoil_Mesh_Size_0.011_Biggest_Airfoil_Mesh_Size_0.06_Initial_Growth_Rate_Domain_0.6_Initial_Growth_Rate_Wing_0.6/mdpas
-#mdpa_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/20191021_170337_AOA_5.0_Wing_Span_4.0_Airfoil_Mesh_Size_0.001_Biggest_Airfoil_Mesh_Size_0.05_Initial_Growth_Rate_Domain_0.7_Initial_Growth_Rate_Wing_0.7/mdpas
-#mdpa_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/20191017_201158_AOA_0.0_Wing_Span_4.0_Airfoil_Mesh_Size_0.01_Biggest_Airfoil_Mesh_Size_0.05_Initial_Growth_Rate_Domain_0.7_Initial_Growth_Rate_Wing_0.7/mdpas
-#mdpa_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/20191021_102917_AOA_5.0_Wing_Span_4.0_Airfoil_Mesh_Size_0.001_Biggest_Airfoil_Mesh_Size_0.05_Initial_Growth_Rate_Domain_0.7_Initial_Growth_Rate_Wing_0.7/mdpas
 gid_output_path=$input_dir_path/output_gid
-#gid_output_path=/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/20191014_112855_AOA_0.0_Wing_Span_4.0_Airfoil_Mesh_Size_0.01_Biggest_Airfoil_Mesh_Size_0.05_Initial_Growth_Rate_Domain_0.7_Initial_Growth_Rate_Wing_0.7/output_salome
 
 DATE=`date '+%Y%m%d_%H%M%S'`
 FILEK=${input_dir_path}/plots/output_terminal_kratos.txt
