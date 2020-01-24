cf5fa3903874114ae1ccdc35d2daa61fc8156b60
Inigo Lopez <inigolcanalejo@gmail.com>
2020-01-24 13:10:49 +0100

    cleaning scripts

--- MeshRefinement3D/scripts/preamble.sh
+++ MeshRefinement3D/scripts/preamble.sh
@@ -10,8 +10,6 @@ source /home/inigo/Documents/paths/kratosMaster4.sh
 echo "PYTHONPATH = $PYTHONPATH"
 echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 
-GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-
 # Path where mdpas and outputs are created
 input_dir_path=/media/inigo/10740FB2740F9A1C/3d_results_test
 
@@ -24,6 +22,7 @@ mdpa_path=$input_dir_path/mdpas
 gid_output_path=$input_dir_path/output_gid
 
 DATE=`date '+%Y%m%d_%H%M%S'`
+GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 FILEK=${input_dir_path}/plots/output_terminal_kratos.txt
 FILES=${input_dir_path}/plots/output_terminal_salome.txt
 FILEC=${input_dir_path}/plots/output_terminal_converter.txt
