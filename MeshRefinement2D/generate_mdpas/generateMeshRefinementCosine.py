81515f7b691a270833f6ca37451d6038bdf802b1
Inigo Lopez <inigolcanalejo@gmail.com>
2021-02-08 16:55:20 +0100

    adjusting edges order

--- MeshRefinement2D/generate_mdpas/generateMeshRefinementCosine.py
+++ MeshRefinement2D/generate_mdpas/generateMeshRefinementCosine.py
@@ -96,6 +96,7 @@ for k in range(Number_Of_Domains_Size):
 
             #Rotate around center to AOA
             geompy.Rotate(Face_Airfoil, OZ, -AOA*math.pi/180.0)
+            geompy.TranslateDXDYDZ(Face_Airfoil, 0.5, 0, 0)
 
             #Create domain
             Face_Domain = geompy.MakeFaceHW(Domain_Length, Domain_Width, 1)
@@ -104,7 +105,7 @@ for k in range(Number_Of_Domains_Size):
             Cut_Domain = geompy.MakeCutList(Face_Domain, [Face_Airfoil], True)
 
             #Explode edges
-            [Edge_1,Edge_LowerSurface_LE,Edge_UpperSurface_LE,Edge_2,Edge_7,Edge_LowerSurface_TE,Edge_UpperSurface_TE,Edge_8] = geompy.ExtractShapes(Cut_Domain, geompy.ShapeType["EDGE"], True)
+            [Edge_1,Edge_2,Edge_7,Edge_LowerSurface_LE,Edge_UpperSurface_LE,Edge_LowerSurface_TE,Edge_UpperSurface_TE,Edge_8] = geompy.ExtractShapes(Cut_Domain, geompy.ShapeType["EDGE"], True)
 
             [Auto_group_for_Sub_mesh_1,Auto_group_for_Sub_mesh_1_1] = geompy.ExtractShapes(Edge_LowerSurface_TE, geompy.ShapeType["VERTEX"], True)
 
