0b656703fa38bc2c4cbb2f0619b66d49d3343724
Inigo Lopez <inigolcanalejo@gmail.com>
2021-05-27 19:53:00 +0200

    running case 1

--- MeshRefinement3D/runKratos/potential_flow_analysis_refinement.py
+++ MeshRefinement3D/runKratos/potential_flow_analysis_refinement.py
@@ -21,7 +21,7 @@ class PotentialFlowAnalysisRefinement(PotentialFlowAnalysis):
 
     def Run(self):
         counter = 1
-        for _ in range(1):
+        for _ in range(100):
             print('\nRUN NUMBER = ', counter)
             counter += 1
             self.SetParameters()
@@ -71,7 +71,7 @@ class PotentialFlowAnalysisRefinement(PotentialFlowAnalysis):
         self.Initial_Growth_Rate_Domain = TBD
         self.Growth_Rate_Domain_Refinement_Factor = TBD
 
-        self.case = 0
+        self.case = 1
         self.Domain_Length = 100
         self.Domain_Width = self.Domain_Length
         self.FarField_MeshSize = int(self.Domain_Length / 10.0)
--- MeshRefinement3D/settings/parameters.sh
+++ MeshRefinement3D/settings/parameters.sh
@@ -1,7 +1,7 @@
 #Parameters
 Number_Of_AOAS=1
 Number_Of_Domains_Refinements=1
-Number_Of_Wing_Refinements=5
+Number_Of_Wing_Refinements=1
 
 Initial_AOA=3.06
 AOA_Increment=1.0
@@ -12,6 +12,6 @@ Growth_Rate_Domain_Refinement_Factor=1.0
 Initial_Growth_Rate_Wing=0.2
 Growth_Rate_Wing_Refinement_Factor=1.0
 
-Smallest_Airfoil_Mesh_Size=0.016
+Smallest_Airfoil_Mesh_Size=0.008
 Biggest_Airfoil_Mesh_Size=0.016
 Wing_span=1.2
