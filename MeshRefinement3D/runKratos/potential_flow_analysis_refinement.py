6374c439b6d00bc3d3a8ede571a4878206a36dda
Inigo Lopez <inigolcanalejo@gmail.com>
2021-06-02 18:40:38 +0200

    generating reference

--- MeshRefinement3D/runKratos/potential_flow_analysis_refinement.py
+++ MeshRefinement3D/runKratos/potential_flow_analysis_refinement.py
@@ -271,10 +271,10 @@ class PotentialFlowAnalysisRefinement(PotentialFlowAnalysis):
             self.minimum_mesh_growth_rate)
         self.project_parameters["processes"]["boundary_conditions_process_list"][2]["Parameters"]["case"].SetInt(self.case)
 
-        # reference_file_path = '/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/2021_05_27_43_naca0012_test/kratos_tests_reference_results'
-        # input_file_name = reference_file_path + '/naca0012_wing_case_' + str(self.case) + '_reference.json'
-        # self.project_parameters["processes"]["auxiliar_process_list"][0]["Parameters"]["input_file_name"].SetString(
-        #     input_file_name)
+        reference_file_path = '/media/inigo/10740FB2740F9A1C/Results/06_wing_mesh_refinement/2021_05_27_43_naca0012_test/kratos_tests_reference_results'
+        input_file_name = reference_file_path + '/naca0012_wing_case_' + str(self.case) + '_reference_stl_wide_wake.json'
+        self.project_parameters["processes"]["auxiliar_process_list"][0]["Parameters"]["output_file_name"].SetString(
+            input_file_name)
 
         # self.project_parameters["processes"]["boundary_conditions_process_list"][2]["Parameters"]["airfoil_meshsize"].SetDouble(
         #     self.Airfoil_MeshSize)
