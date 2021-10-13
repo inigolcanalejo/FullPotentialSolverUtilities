3d55cb09369ea497c0e23d93601b16c68c558223
Iñigo López Canalejo <inigo.lopez@tum.de>
2021-10-13 14:35:44 +0200

    adding experiment plot

--- MeshRefinement3D/runKratos/write_forces_process.py
+++ MeshRefinement3D/runKratos/write_forces_process.py
@@ -565,6 +565,13 @@ class WriteForcesProcess(ComputeLiftProcess):
                     cp_potential = [float(line.split()[1]) for line in open(cp_tranair_file_name).readlines() if len(line.split()) > 0]
                     plt.plot(x_potential,cp_potential,'bx',label='TRANAIR', markersize=5)
 
+                # Experiment (wind tunnel data)
+                cp_ntf_file_name = 'references/cp/crm/ntf/ntf_' + str(section) + '.dat'
+                if os.path.exists(cp_ntf_file_name):
+                    x_potential = [float(line.split()[0]) for line in open(cp_ntf_file_name).readlines() if len(line.split()) > 0]
+                    cp_potential = [float(line.split()[1]) for line in open(cp_ntf_file_name).readlines() if len(line.split()) > 0]
+                    plt.plot(x_potential,cp_potential,'ks',label='WTD', markersize=5)
+
                 title="y/b: %.2f, $M$: %.2f, Cl: %.4f, Cd: %.4f, Clref: %.4f, Cdref: %.4f," % (section/10000.0, self.mach, self.lift_coefficient, self.drag_coefficient, self.cl_reference, self.cd_reference)
                 #title="y/b: %.2f, $M$: %.2f, $\mu$: %.2f, $M_c$: %.2f" % (section/100.0, self.mach, self.ufc, self.cm)
                 plt.title(title)
